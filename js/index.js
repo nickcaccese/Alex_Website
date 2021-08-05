382d29cf1e7acbb51e8dba12a9c4248953b556a2
Nick Caccese <nickcaccese@gmail.com>
2021-08-05 19:33:57 -0400

    Fixed the space between Previous lessons and Lesson 2

--- js/index.js
+++ js/index.js
@@ -5,10 +5,13 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    //Add a line of code to remove and add the margin to the parent element when clicked
     if (content.style.display === "block") {
       content.style.display = "none";
+      this.style["margin-bottom"] = "1em";
     } else {
       content.style.display = "block";
+      this.style["margin-bottom"] = "0";
     }
   });
 }
